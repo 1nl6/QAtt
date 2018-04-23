7e5e92c93ccd7e0260edd2ad0e04a935e78ef280
Niki Lin <niki@Nikis-MacBook-Pro.local>
2018-04-23 13:24:47 -0400

    confirmation dialog

--- app/src/main/java/com/example/qatt/DropDatabase.java
+++ app/src/main/java/com/example/qatt/DropDatabase.java
@@ -1,9 +1,13 @@
 package com.example.qatt;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.qatt.database.DaoApplication;
 import com.example.qatt.database.ScanRepository;
@@ -26,11 +30,28 @@ public class DropDatabase extends AppCompatActivity {
     }
 
     public void drop(View view){
-        ScanRepository.clearScan(this.getApplicationContext());
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("You'll lose all you scan records!")
+                .setTitle("Drop attendance database?");
+        builder.setPositiveButton("Drop", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dropDB();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
 
+    }
+
+    public void dropDB(){
+        ScanRepository.clearScan(this.getApplicationContext());
+        Toast.makeText(this,"Attendance database has been dropped.", Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
-
-
     }
 }
